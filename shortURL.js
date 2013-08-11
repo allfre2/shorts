dec471f3d9ab7f84512dfffc32846c552ef356f7
allfre2 <allfre2luzon@gmail.com>
2013-08-11 16:39:08 -0400

    changed title

--- shortURL.js
+++ shortURL.js
@@ -18,7 +18,7 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
-var title = "shortURL"
+var title = "shorts"
 var googl = document.getElementById("_");
 var APIUrl = "https://www.googleapis.com/urlshortener/v1/url";
 var xhr = new XMLHttpRequest();
